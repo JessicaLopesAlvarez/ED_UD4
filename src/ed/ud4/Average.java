620d6fd4fea625a7d961623062eb9ddfdd71d057
JessicaLopesAlvarez <jlopesa@iesch.org>
2024-11-05 18:20:23 +0100

    todos los cambios del 6-9

--- src/ed/ud4/Average.java
+++ src/ed/ud4/Average.java
@@ -3,21 +3,23 @@ package ed.ud4;
 import java.util.Random;
 
 public class Average {
+    static final int COUNT=20;
+    static final int MAX=100;
     public static void main(String[] args) {
         Random r = new Random(System.currentTimeMillis());
-        int count = r.nextInt(20);
-    
+        int count = r.nextInt(COUNT);
         int total = 0;
-        for (int i=0; i<count;i++){
-            int n = r.nextInt(100) +1;
-            total+= n;
-            System.out.println(n+" ");
-        }        
-        System.out.println();
-        System.out.println("Total: "+total);
-        //Cambiamos dobles por enteros
-        int media = (int) total / (int) count;
-        System.out.println("Media: " + media);
+
+        for (int i=0; i<count; i++){
+            int a = r.nextInt(MAX) +1;
+            total+= a;
+            System.out.println(a + " ");
+        }         
+        System.out.println("Count = " + count);
+        System.out.println("Total = " + total);
+        double average = (double) total / count;
+        System.out.println("Average = " + average);
+        System.out.println("End of program");
         
     }
 }
